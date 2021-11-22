91409657ba06600edfe70b1e5557697dceb00523
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-11-22 11:12:32 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from presets import Presets
 from pyrogram.types import CallbackQuery
 
-@Bot.on_callback_query(filters.regex(r'^about_btn$'))
+@Bot.on_callback_query(filters.regex(r'^how_btn$'))
 async def help_about_button(c: Bot, cb: CallbackQuery):
     await cb.answer()
     await cb.message.edit(Presets.HOW_TXT, reply_markup=back_button, disable_web_page_preview=True)
