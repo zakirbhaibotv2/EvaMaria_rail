5927cf1ed13f0814d29a12d170a851395b5fab77
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-11-22 10:55:56 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -1,1 +1,8 @@
 
+elif update.data == "confirm_reset":
+        await db.delete_user(update.from_user.id)
+        await db.add_user(update.from_user.id)
+        await update.message.edit_text(
+            text="**Settings reset successfully**",
+            disable_web_page_preview=True,
+            reply_markup=HELP_BUTTONS
