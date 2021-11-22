ddc4829d5ab44280803a060b06826ee53fa00cc3
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-11-22 22:29:51 +0530

    Update buttons.py

--- suport/buttons.py
+++ suport/buttons.py
@@ -1,1 +1,10 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+start = [
+    [
+        InlineKeyboardButton("〽️ MAIN CHANNEL 〽️", url="https://t.me/joinchat/Fhcssgw5H8wwMDBl")
+    ],
+    [
+        InlineKeyboardButton("🔰 SERIES", url="https://t.me/netflixorgi")
+    ]
+    
