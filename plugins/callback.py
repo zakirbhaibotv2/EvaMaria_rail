d729f91a5132cd193097715e3617e882af2bd4f7
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-11-22 12:37:15 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -5,4 +5,4 @@ from pyrogram.types import CallbackQuery
 @Client.on_callback_query(filters.regex(r'^how_btn$'))
 async def help_about_button(c: Client, cb: CallbackQuery):
     await cb.answer()
-    await cb.message.edit(Presets.HOW_TXT, disable_web_page_preview=True)
+    await cb.message.edit(Presets.HOW_TXT, show_alert=True)
