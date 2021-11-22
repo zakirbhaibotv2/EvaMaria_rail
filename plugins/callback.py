aa422e453ca6e667d4761be7bd25e5ed73b57474
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-11-22 22:23:26 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -1,8 +1,9 @@
 from pyrogram import Client, filters
 from presets import Presets
 from pyrogram.types import CallbackQuery
+from suport.buttons import support_btn
 
 @Client.on_callback_query(filters.regex(r'^how_btn$'))
 async def help_about_button(c: Client, cb: CallbackQuery):
     await cb.answer()
-    await cb.message.edit(Presets.HOW_TXT, show_alert=True)
+    await cb.message.edit(Presets.HOW_TXT, reply_markup=support_btn)
