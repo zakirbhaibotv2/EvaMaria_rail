8bd5e4ce8a18fe7acbc627b3190bb428df5729bc
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-11-23 21:09:39 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,2 +1,2 @@
 class Presets(object):
-  HOW_TXT = "Just send any Movie ➤ Example: Home , Inception ..\n\n #ProTip: Copy paste Movie name from Google."
+  HOW_TXT = "Just send any Movie name\n Example: Home , Inception ..\n\n #ProTip: Copy paste Movie name from Google."
