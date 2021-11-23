5c670186638de26ca1df1f1a795e4d1f2cef0660
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-11-23 20:45:31 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -6,4 +6,4 @@ from suport.buttons import support_btn
 @Client.on_callback_query(filters.regex(r'^how_btn$'))
 async def help_about_button(c: Client, cb: CallbackQuery):
     await cb.answer()
-    await cb.message.edit(Presets.HOW_TXT, reply_markup=support_btn, disable_web_page_preview=True)
+    await cb.message.edit(Presets.HOW_TXT, reply_markup=support_btn)
