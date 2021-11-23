b3a414122711a34429c798b5ed2cc558c07e4013
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-11-24 02:12:49 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -50,7 +50,7 @@ async def save_group(bot, message):
             if (temp.MELCOW).get('welcome') is not None:
                 try:
                     await (temp.MELCOW['welcome']).delete()
-                    count = app.get_chat_members_count("pyrogramchat")
+                    count = app.get_chat_members_count("malayayalies")
                 except:
                     pass
             temp.MELCOW['welcome'] = await message.reply_audio("https://hybridf2l.herokuapp.com/13a/4_5951903479080422091.ogg",
