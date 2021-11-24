5591cfae79b581d5d68f46f9ff5633c496f1a275
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-11-24 23:14:40 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -22,10 +22,28 @@ async def help_about_button(c: Client, cb: CallbackQuery):
 async def help_about_button(c: Client, cb: CallbackQuery):
     await cb.answer()
     await cb.message.edit(Presets.GAME_TXT, disable_web_page_preview=True,
-                          reply_markup=InlineKeyboardMarkup[[
-            InlineKeyboardButton('〽️ MAIN CHANNEL 〽️', url='https://t.me/joinchat/Fhcssgw5H8wwMDBl')
-            InlineKeyboardButton('🔰 SERIES', url='https://t.me/netflixorgi')
-            ],[
-            InlineKeyboardButton('🔐 Close', callback_data='close_data')
-        ]]
-   
+                          reply_markup=InlineKeyboardMarkup(
+            [
+                [  # First row
+                    InlineKeyboardButton(  # Generates a callback query when pressed
+                        "▶ Play - MotoFX 2",
+                        url="https://prizes.gamee.com/game-bot/motofx2"
+                    ),
+                    InlineKeyboardButton(  # Opens a web URL
+                        "▶ Play - Little Plane",
+                        url="https://prizes.gamee.com/game-bot/5IsYwla"
+                    ),
+                ],
+                [  # Second row
+                    InlineKeyboardButton(  # Opens the inline interface
+                        "▶ Play - Ride or Die",
+                        url="https://prizes.gamee.com/game-bot/rideordie"
+                    ),
+                    InlineKeyboardButton(  # Opens the inline interface in the current chat
+                        "Back",
+                        callback_data="pyrogram"
+                    )
+                ]
+            ]
+        )
+    )
