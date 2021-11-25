c33dd17e57a6d8a8d8f5e5a567446b0f9e12ef94
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-11-25 16:57:46 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,5 +1,5 @@
 class Presets(object):
   HOW_TXT = "Just send any Movie name\n ➤Example: Kala , Inception ..\n\n #ProTip: Copy paste Movie name from Google."
-  GAME_TXT = " 𝖥𝗎𝗇 𝖬𝗈𝖽𝗎𝗅𝖾:\n 𝖭𝗈𝗍𝗁𝗂𝗇𝗀 𝖬𝗎𝖼𝗁 𝖩𝗎𝗌𝗍 𝖲𝗈𝗆𝖾 𝖫𝗂𝗍𝗍𝗅𝖾 𝖳𝗂𝗆𝖾𝖯𝖺𝗌𝗌𝖾𝗌"
+  GAME_TXT = "Fun Module:\n Nothing Much Just Some Little Timepasses"
 
  
