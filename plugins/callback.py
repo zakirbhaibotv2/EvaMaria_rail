9120617f84f19495fa72dd1ca431f7b8166fe881
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-11-25 16:46:04 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -22,7 +22,7 @@ async def help_about_button(c: Client, cb: CallbackQuery):
 @Client.on_callback_query(filters.regex(r'^game_btn$'))
 async def help_about_button(c: Client, cb: CallbackQuery):
     await cb.answer()
-    await cb.edit_message_media(InputMediaPhoto("https://telegra.ph/file/8205571d6ac0c64762e47.jpg"), Presets.GAME_TXT,
+    await cb.message.edit(InputMediaPhoto("https://telegra.ph/file/8205571d6ac0c64762e47.jpg"), Presets.GAME_TXT, disable_web_page_preview=True,
                           reply_markup=InlineKeyboardMarkup(
             [
                 [  # First row
