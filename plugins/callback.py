f503cb3e222488eae53b464ba3aac001917247af
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-12-07 21:12:00 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -22,9 +22,9 @@ async def help_about_button(c: Client, cb: CallbackQuery):
 @Client.on_callback_query(filters.regex(r'^game_btn$'))
 async def help_about_button(c: Client, cb: CallbackQuery):
     await cb.answer()
-    await cb.message.edit_media("AgACAgUAAxkBAAOxYa960mGm_2mWhV1Ni76uvPqAU0wAAie1MRvV_fFUmSI9bBKs8xABAAMCAANzAAMjBA", Presets.HOW_TXT,
-                          reply_markup=InlineKeyboardMarkup([
-                           [InlineKeyboardButton("〽️MAIN CHANNEL 〽️", url="https://t.me/joinchat/Fhcssgw5H8wwMDBl")],
-                           [InlineKeyboardButton("🔰 SERIES", url="https://t.me/netflixorgi")]
-                       ]))
+    await cb.message.edit_media("AgACAgUAAxkBAAOxYa960mGm_2mWhV1Ni76uvPqAU0wAAie1MRvV_fFUmSI9bBKs8xABAAMCAANzAAMjBA", Presets.HOW_TXT)
+                          #reply_markup=InlineKeyboardMarkup([
+                           #[InlineKeyboardButton("〽️MAIN CHANNEL 〽️", url="https://t.me/joinchat/Fhcssgw5H8wwMDBl")],
+                           #[InlineKeyboardButton("🔰 SERIES", url="https://t.me/netflixorgi")]
+                       #]))
                           
