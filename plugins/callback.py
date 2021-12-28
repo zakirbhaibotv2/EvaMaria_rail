6fdb70986902a75aef07b7743db03ad3705de307
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-12-28 21:01:56 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -24,9 +24,11 @@ async def help_about_button(c: Client, cb: CallbackQuery):
                            [InlineKeyboardButton("🔰 SERIES", url="https://t.me/netflixorgi")]
                        ]))
 
-@Client.on_callback_query(filters.regex(r'^game_btn$'))
+@Client.on_callback_query(filters.regex(r'^how_btn$'))
 async def help_about_button(c: Client, cb: CallbackQuery):
     await cb.answer()
-    await cb.message.edit_media(InputMediaPhoto("https://telegra.ph/file/8205571d6ac0c64762e47.jpg", caption = "Fun Module:\n Nothing Much Just Some Little Timepasses", reply_markup = Buttonpt))
-                          
-                          
+    await cb.message.edit(Presets.HOW_TXT, disable_web_page_preview=True,
+                          reply_markup=InlineKeyboardMarkup([
+                           [InlineKeyboardButton("Music 🎶", url="https://t.me/joinchat/Fhcssgw5H8wwMDBl")],
+                           [InlineKeyboardButton("Games 🎮", url="https://t.me/netflixorgi")]
+                       ]))
