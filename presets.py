a72b4fa13c874a8abdf8ad919ad37f7b5a161987
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-12-29 17:52:20 +0530

    Update presets.py

--- presets.py
+++ presets.py
@@ -1,9 +1,6 @@
 class Presets(object):
   HOW_TXT = "Just send any Movie name\n ➤Example: Kala , Inception ..\n\n #ProTip: Copy paste Movie name from Google."
   GAME_TXT = "Fun Module:\n Nothing Much Just Some Little Timepasses"
-  MUSIC_TXT = "🎼Song Download🎼\nSong Download Module, For Those Who Love Music\n\n
-               🎈 Command 🎈\n\n
-               - /song [Song Name] - To Download Music.\n\n
-               @malayayalies"
+  MUSIC_TXT = "🎼Song Download🎼\nSong Download Module, For Those Who Love Music\n\n🎈 Command 🎈\n\n- /song [Song Name] - To Download Music.\n\n@malayayalies"
 
  
