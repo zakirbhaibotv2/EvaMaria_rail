79fac6b21d868ca6557b80ef0d01d1f360743d46
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-12-29 16:45:50 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -278,7 +278,7 @@ def time_to_seconds(time):
     stringt = str(time)
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
 
-@app.on_message(filters.command('dsong'))
+@Client.on_message(filters.command('dsong'))
 def song(client, message):
 
     user_id = message.from_user.id 
