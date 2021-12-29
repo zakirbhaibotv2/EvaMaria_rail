1960fafe05f79550c34781349b9bbf648bf1654f
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-12-29 15:53:34 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -29,5 +29,3 @@ async def help_about_button(c: Client, cb: CallbackQuery):
     await cb.answer()
     await cb.message.edit(Presets.GAME_TXT, disable_web_page_preview=True,
                           reply_markup=InlineKeyboardMarkup(
-                              [
-                                  jjj
