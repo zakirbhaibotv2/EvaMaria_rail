fa4637146f5d6f7a78a240dda333cea746e18642
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-12-29 15:44:54 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -28,12 +28,33 @@ async def help_about_button(c: Client, cb: CallbackQuery):
 async def help_about_button(c: Client, cb: CallbackQuery):
     await cb.answer()
     await cb.message.edit(Presets.GAME_TXT, disable_web_page_preview=True,
-                          reply_markup=InlineKeyboardMarkup[[
-            InlineKeyboardButton('Lumber Jack', url='https://tbot.xyz/lumber/#eyJ1Ijo2MTcyNzA3MDIsIm4iOiJULkcgVGFubmhhdXMgIiwiZyI6Ikx1bWJlckphY2siLCJjaSI6IjM5MzY2NzY4MDM4NDk5NDk3OTciLCJpIjoiQlFBQUFHRUZBUUN1emNva3pHMFFmREc2T1NvIn0xM2QxNDI3MWNmMTFjODMxMWMyYWIyNzExZWQ4NGMzO'),
-            InlineKeyboardButton('Corasirs', url='https://tbot.xyz/corsairs/#eyJ1Ijo2MTcyNzA3MDIsIm4iOiJULkcgVGFubmhhdXMgIiwiZyI6IkNvcnNhaXJzIiwiY2kiOiIzOTM2Njc2ODAzODQ5OTQ5Nzk3IiwiaSI6IkJRQUFBRjRGQVFDdXpjb2txQkRJU2xXdmF6cyJ9MWY0NzgxYzBjMzJkOTM5OWNkYjRlMjZlZDgxODUyMGU')
-            ],[
-            InlineKeyboardButton('Math Battle', url='https://tbot.xyz/math/#eyJ1Ijo2MTcyNzA3MDIsIm4iOiJULkcgVGFubmhhdXMgIiwiZyI6Ik1hdGhCYXR0bGUiLCJjaSI6IjM5MzY2NzY4MDM4NDk5NDk3OTciLCJpIjoiQlFBQUFGY0ZBUUN1emNva3JUWTdMQTgxOTBzIn0yODk2YTk1YzI5ZGVlODg4ZjZjN2Q5ZDQyOTExNGQyZg')
-            ],[
-            InlineKeyboardButton('🔙 Back', callback_data='start'),
-            InlineKeyboardButton('🔒 Close', callback_data='close_data')
-            ]]
+                          reply_markup=InlineKeyboardMarkup(
+            [
+                [  # First row
+                    InlineKeyboardButton(  # Generates a callback query when pressed
+                        "Lumber Jack",
+                        url='https://tbot.xyz/lumber/'
+                    ),
+                    InlineKeyboardButton(  # Opens a web URL
+                        "Corasirs",
+                        url="https://tbot.xyz/corsairs/"
+                    ),
+                ],
+                [
+                    InlineKeyboardButton(
+                         "Math Battle"
+                         url='https://tbot.xyz/math/'
+                    ),
+                ],
+                [  # Second row
+                    InlineKeyboardButton(  # Opens the inline interface
+                        "🔙 Back",
+                        callback_data="start"
+                    ),
+                    InlineKeyboardButton(  # Opens the inline interface in the current chat
+                        "🔒 Close",
+                        callback_data="Close_data"
+                    )
+                ]
+            ]
+        )
