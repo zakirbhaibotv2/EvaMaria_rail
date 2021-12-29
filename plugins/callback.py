2eef9da4cd20a16df5827e79bd2444269323d93f
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-12-29 10:44:03 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -29,6 +29,6 @@ async def help_about_button(c: Client, cb: CallbackQuery):
     await cb.answer()
     await cb.message.edit(Presets.GAME_TXT, disable_web_page_preview=True,
                           reply_markup=InlineKeyboardMarkup([
-                           [InlineKeyboardButton("Music 🎶", url="https://t.me/joinchat/Fhcssgw5H8wwMDBl")],
-                           [InlineKeyboardButton("Games 🎮", url="https://t.me/netflixorgi")]
+                           [InlineKeyboardButton("Lumber Jack", url="https://tbot.xyz/lumber/#eyJ1Ijo2MTcyNzA3MDIsIm4iOiJULkcgVGFubmhhdXMgIiwiZyI6Ikx1bWJlckphY2siLCJjaSI6IjM5MzY2NzY4MDM4NDk5NDk3OTciLCJpIjoiQlFBQUFHRUZBUUN1emNva3pHMFFmREc2T1NvIn0xM2QxNDI3MWNmMTFjODMxMWMyYWIyNzExZWQ4NGMzO"),
+                           [InlineKeyboardButton("Corasirs", url="https://tbot.xyz/corsairs/#eyJ1Ijo2MTcyNzA3MDIsIm4iOiJULkcgVGFubmhhdXMgIiwiZyI6IkNvcnNhaXJzIiwiY2kiOiIzOTM2Njc2ODAzODQ5OTQ5Nzk3IiwiaSI6IkJRQUFBRjRGQVFDdXpjb2txQkRJU2xXdmF6cyJ9MWY0NzgxYzBjMzJkOTM5OWNkYjRlMjZlZDgxODUyMGU")]]
                        ]))
