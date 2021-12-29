2c190b54166aac81a01816ebca3d8aba09418f7a
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-12-29 17:05:55 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -310,13 +310,13 @@ def song(client, message):
         )
         print(str(e))
         return
-    m.edit("**Downloading the song by @SDbotsz**")
+    m.edit("This May Take Some Time\n**Downloading the song by @malayayalies**")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**Uploaded** by @SDbotsz.**'
+        rep = '**Uploaded** by @malayayalies.**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
