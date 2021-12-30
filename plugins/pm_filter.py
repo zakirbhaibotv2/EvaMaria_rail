1491b8624413879ad40d23ff31fadf87b035bcce
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-12-30 19:59:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -439,7 +439,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('🔰 SERIES', url='https://t.me/netflixorgi')
             ],[
             InlineKeyboardButton('♀️ 2nd GROUP', url='https://t.me/malayalam_cinmaa'),
-            InlineKeyboardButton('🧩 FUN', callback_data='gum_btn')
+            InlineKeyboardButton('🧩 FUN', callback_data='music_btn')
             ],[
             InlineKeyboardButton('Music 🎶', callback_data='music_btn')
             ],[
