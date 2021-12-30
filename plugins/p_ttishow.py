684855724d7d7aa29ab016574bd3575ce7acca81
Vasubai <90282707+Vasubai@users.noreply.github.com>
2021-12-30 21:14:00 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -52,7 +52,7 @@ async def save_group(bot, message):
             text=f"<b>Thankyou For Adding Me In {message.chat.title} ❣️\n\nIf you have any questions & doubts about using me contact support.</b>",
             reply_markup=reply_markup)
     else:
-        audio_file = "CQACAgQAAxkBAAPNYc3QTMD6Yh6VAp3mrebsG-g-fdgAAssKAAKuaJlSoYO8NvI-MHUjBA"
+        audio_file = "https://sndup.net/kryf/d"
         thumbnail = "https://telegra.ph/file/cdc605b8675b0226bcfb3.jpg"
         total=await bot.get_chat_members_count(message.chat.id)
         for u in message.new_chat_members:
