62652bc5ff7b1026da99b2cb33b5f457bea27684
zakirbhaibotv2 <90923366+zakirbhaibotv2@users.noreply.github.com>
2022-01-16 11:12:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Vasubai/EvaMaria.git /EvaMaria
+  git clone https://github.com/zakirbhaibotv2/EvaMaria_rail.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
